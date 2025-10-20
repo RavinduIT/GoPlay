2bf03eaa1fc4cb6e3d2fac0d6fdafdf9936d27f6
Piyumi_19 <piyumiwk@gmail.com>
2025-10-20 17:12:33 +0530

    updated the shop dashboard

--- app/views/shop-owner/sidebar.php
+++ app/views/shop-owner/sidebar.php
@@ -46,20 +46,7 @@ $additionalJS = ['/public/js/pages/shop owner/sidebar.js'];
                     
                 </a>
             </li>
-            <!--<li>
-                <a href="/shop-owner/sales">
-                    <i class="fas fa-chart-line"></i>
-                    <span>Sales</span>
-                </a>
-            </li>
-            <li>
-                <a href="/shop-owner/customers">
-                    <i class="fas fa-users"></i>
-                    <span>Customers</span>
-                    <span class="badge">234</span>
-                </a>
-            </li>-->
-            <li>
+            <li class="<?= strpos($_SERVER['REQUEST_URI'], '/shop-owner/reviews') !== false ? 'active' : '' ?>">
                 <a href="/shop-owner/reviews">
                     <i class="fas fa-star"></i>
                     <span>Reviews</span>
