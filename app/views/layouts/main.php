e2ca88e19f87cc297f953ced0d5bdd23a0de9a79
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-10-20 18:56:05 +0530

    implemented stylings

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -7,8 +7,7 @@
     
     <!-- CSS -->
      <link rel="stylesheet" href="/public/css/components/navbar.css">
-    
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> 
     <link rel="stylesheet" href="/public/css/pages/news-index.css">
     
     <?php if (isset($additionalCSS)): ?>
