c24b1a5df517aa50fd7c8783d997705c9eec6116
Piyumi_19 <piyumiwk@gmail.com>
2026-01-11 09:51:54 +0530

    some changes in dashboard

--- app/views/shop-owner/dashboard.php
+++ app/views/shop-owner/dashboard.php
@@ -30,21 +30,21 @@ $additionalJS = ['/public/js/pages/shop-owner-dashboard.js'];
                     <a href="/shop-owner/products">
                         <i class="fas fa-box"></i>
                         <span>Products</span>
-                        <span class="badge">156</span>
+                        
                     </a>
                 </li>
                 <li>
                     <a href="/shop-owner/orders">
                         <i class="fas fa-shopping-cart"></i>
                         <span>Orders</span>
-                        <span class="badge new">12</span>
+                        
                     </a>
                 </li>
                 <li>
                     <a href="/shop-owner/inventory">
                         <i class="fas fa-warehouse"></i>
                         <span>Inventory</span>
-                        <span class="badge warning">5</span>
+                        
                     </a>
                 </li>
                 <li>
@@ -53,18 +53,12 @@ $additionalJS = ['/public/js/pages/shop-owner-dashboard.js'];
                         <span>Sales</span>
                     </a>
                 </li>
-                <li>
-                    <a href="/shop-owner/customers">
-                        <i class="fas fa-users"></i>
-                        <span>Customers</span>
-                        <span class="badge">234</span>
-                    </a>
-                </li>
+                
                 <li>
                     <a href="/shop-owner/reviews">
                         <i class="fas fa-star"></i>
                         <span>Reviews</span>
-                        <span class="badge">45</span>
+                        
                     </a>
                 </li>
                 <li class="nav-divider"></li>
