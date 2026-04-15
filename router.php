418602d611b25c3e0737390f44821262f9d3792a
W.T Mahagmage <winukauni@gmail.com>
2026-04-15 20:47:25 +0530

    fix devops error

--- router.php
+++ router.php
@@ -6,8 +6,19 @@
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Serve static files directly
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+// Serve static files directly.
+// Support both /public/... URLs and root-relative /... URLs.
+$staticFile = null;
+
+if ($uri !== '/') {
+    if (strpos($uri, '/public/') === 0) {
+        $staticFile = __DIR__ . $uri;
+    } else {
+        $staticFile = __DIR__ . '/public' . $uri;
+    }
+}
+
+if ($staticFile && is_file($staticFile)) {
     return false;
 }
 
